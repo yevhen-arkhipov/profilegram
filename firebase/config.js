b7ca53bbfad7be9ddb8a88f5e8c3caeaf66ca22b
Yevhen Arkhipov <arkhipovyevhen@gmail.com>
2023-04-21 17:37:55 +0300

    firebase-fix

--- firebase/config.js
+++ firebase/config.js
@@ -1,22 +1,19 @@
-import { initializeApp, getApp } from "firebase/app";
-import { getAuth } from "firebase/auth";
-import { getFirestore } from "firebase/firestore";
-import { getStorage } from "firebase/storage";
+import { initializeApp, getApp } from 'firebase/app';
+import { getAuth } from 'firebase/auth';
+import { getFirestore } from 'firebase/firestore';
+import { getStorage } from 'firebase/storage';
 
 function initializeAppIfNecessary() {
   try {
     return getApp({});
   } catch (any) {
     const firebaseConfig = {
-      apiKey: "AIzaSyBp59Y3zIggknbnKNpYEphVjKJBH7HMA48",
-      authDomain: "profilegramapp.firebaseapp.com",
-      databaseURL:
-        "https://profilegramapp-default-rtdb.europe-west1.firebasedatabase.app",
-      projectId: "profilegramapp",
-      storageBucket: "profilegramapp.appspot.com",
-      messagingSenderId: "833683349456",
-      appId: "1:833683349456:web:a3c26467d36897b3b023d5",
-      measurementId: "G-4TKF6QQC3Y",
+      apiKey: 'AIzaSyA8_uU2frJT7rz-DKs0TRzttEYfr4NFtXU',
+      authDomain: 'profilegram-82040.firebaseapp.com',
+      projectId: 'profilegram-82040',
+      storageBucket: 'profilegram-82040.appspot.com',
+      messagingSenderId: '30535182610',
+      appId: '1:30535182610:web:481ec03b6187adcb31abcb',
     };
     return initializeApp(firebaseConfig);
   }
